a6ad206a626d9e2241cdc44c8b6e40ef9fc2a692
wfunc <114468522+wfunc@users.noreply.github.com>
2023-05-17 10:27:25 +0800

    Update captcha.go

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -5,6 +5,7 @@ import (
 	"github.com/codingeasygo/web"
 	"github.com/dchest/captcha"
 	"github.com/wfunc/go/define"
+	"github.com/wfunc/go/util"
 )
 
 func CaptchaVerify(id, code string) (err error) {
@@ -48,7 +49,53 @@ func NewCaptchaH(s *web.Session) web.Result {
 	})
 }
 
+//VerifyCaptchaH is http handler
+/**
+ *
+ * @api {GET} /pub/captcha/verify Verify Captcha
+ * @apiName VerifyCaptcha
+ * @apiGroup Captcha
+ *
+ * @apiParam  {String} captcha_id the captcha id
+ * @apiParam  {String} captcha_code the captcha code
+ *
+ * @apiSuccess (200) {Number} code the respnose code, see the common define,
+ *
+ *
+ * @apiParamExample  {Query} Request-Example:
+ * captcha_id=xx&captcha_code=1234
+ *
+ *
+ * @apiSuccessExample {JSON} Success-Response:
+ * {
+ *     "code": 0
+ * }
+ * @apiSuccessExample {JSON} Frequently-Response:
+ * {
+ *     "code": 1600,
+ *     "after": 10000,
+ * }
+ *
+ */
+func VerifyCaptchaH(s *web.Session) web.Result {
+	var captchaID, captchaCode string
+	err := s.ValidFormat(`
+		captcha_id,R|S,L:0;
+		captcha_code,R|S,L:0;
+	`, &captchaID, &captchaCode)
+	if err != nil {
+		return util.ReturnCodeLocalErr(s, define.ArgsInvalid, "arg-err", err)
+	}
+	if !captcha.VerifyString(captchaID, captchaCode) {
+		return util.ReturnCodeLocalErr(s, define.CodeInvalid, "arg-err", define.ErrCodeInvalid)
+	}
+	return s.SendJSON(xmap.M{
+		"code": define.Success,
+	})
+}
+
 func Hand(pre string, mux *web.SessionMux) {
 	mux.HandleFunc("^"+pre+"/pub/captcha/new(\\?.*)?$", NewCaptchaH)
+	mux.HandleFunc("^"+pre+"/pub/captcha/verify(\\?.*)?$", VerifyCaptchaH)
 	mux.HandleNormal("^"+pre+"/pub/captcha/.*$", captcha.Server(captcha.StdWidth, captcha.StdHeight))
 }
